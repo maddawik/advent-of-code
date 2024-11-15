98c57bd61e64f5033101fb9c1304bc426caf0952
maddawik <edwardreillycomputers@gmail.com>
2024-11-15 00:51:32 -0600

    initial commit

---
+++ template/solution.go
@@ -0,0 +1,12 @@
+// Package solution is the solution that solves things!
+package solution
+
+// Part1 returns the solution for day X part 1
+func Part1() int {
+	return 0
+}
+
+// Part2 returns the solution for day X part 2
+func Part2() int {
+	return 1
+}
---
+++ template/solution_test.go
@@ -0,0 +1,41 @@
+package solution
+
+import "testing"
+
+func TestPart1(t *testing.T) {
+	t.Run("sample input", func(t *testing.T) {
+		got := Part1()
+		want := 0
+
+		assertSolution(t, got, want)
+	})
+	t.Run("actual input", func(t *testing.T) {
+		got := Part1()
+		want := 0
+
+		assertSolution(t, got, want)
+	})
+}
+
+func TestPart2(t *testing.T) {
+	t.Run("sample input", func(t *testing.T) {
+		got := Part2()
+		want := 1
+
+		assertSolution(t, got, want)
+	})
+	t.Run("actual input", func(t *testing.T) {
+		got := Part2()
+		want := 1
+
+		assertSolution(t, got, want)
+	})
+}
+
+func assertSolution(t testing.TB, got int, want int) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got %d, wanted %d", got, want)
+	}
+}
